9ed585c9d74d91bc6b5e54234ebb8bd81c36c549
Abass Adamo <adamoabasstope@yahoo.com>
2020-02-15 09:27:32 +0100

    first commit on proxy rotator

---
+++ index.js
@@ -0,0 +1,51 @@
+const request = require('request')
+const cheerio = require('cheerio')
+
+function proxyRotator(){
+let ip_addresses = [];
+let port_numbers = [];
+
+request("https://sslproxies.org/", function(error, response, html) {
+  if (!error && response.statusCode == 200) {
+    const $ = cheerio.load(html);
+
+    $("td:nth-child(1)").each(function(index, value) {
+      ip_addresses[index] = $(this).text();
+    });
+
+    $("td:nth-child(2)").each(function(index, value) {
+      port_numbers[index] = $(this).text();
+    });
+  } else {
+    console.log("Error loading proxy, please try again");
+  }
+
+  ip_addresses.join(", ");
+  port_numbers.join(", ");
+
+  console.log("IP Addresses:", ip_addresses);
+  console.log("Port Numbers:", port_numbers);
+  let random_number = Math.floor(Math.random() * 100)
+  console.log(random_number)
+  console.log(ip_addresses[random_number]);
+  console.log(port_numbers[random_number]);
+  let proxy = `http://${ip_addresses[random_number]}:${port_numbers[random_number]}`;
+ console.log(proxy);
+});
+ }
+ const options = {
+    url:
+      "https://www.foxnews.com/",
+    method: "GET",
+    proxy: proxyRotator()
+  };
+  
+  request(options, function(error, response, html) {
+    if (!error && response.statusCode == 200) {
+      const $ = cheerio.load(html);
+      let article_headings = $("h2").text();
+      console.log(article_headings);
+    } else {
+      console.log("Error scraping site, please try again");
+    }
+  });
